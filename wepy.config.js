b342b50da3946b3808471fc064c3f0d2d2964f00
markalex25 <markalex25@163.com>
2017-09-09 14:55:06 +0800

    2048小游戏

--- wepy.config.js
+++ wepy.config.js
@@ -2,65 +2,65 @@ const path = require('path');
 var prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  wpyExt: '.wpy',
-  build: {
-    web: {
-      htmlTemplate: path.join('src', 'index.template.html'),
-      htmlOutput: path.join('web', 'index.html'),
-      jsOutput: path.join('web', 'index.js')
-    }
-  },
-  eslint: false,
-  compilers: {
-    less: {
-      compress: false
+    wpyExt: '.wpy',
+    build: {
+        web: {
+            htmlTemplate: path.join('src', 'index.template.html'),
+            htmlOutput: path.join('web', 'index.html'),
+            jsOutput: path.join('web', 'index.js')
+        }
     },
-    /*sass: {
-      outputStyle: 'compressed'
-    },*/
-    babel: {
-      sourceMap: true,
-      presets: [
-        'es2015',
-        'stage-1'
-      ],
-      plugins: [
-        'transform-decorators-legacy',
-        'transform-export-extensions',
-        'syntax-export-extensions'
-      ]
+    eslint: false,
+    compilers: {
+        less: {
+            compress: false
+        },
+        /*sass: {
+          outputStyle: 'compressed'
+        },*/
+        babel: {
+            sourceMap: true,
+            presets: [
+                'es2015',
+                'stage-1'
+            ],
+            plugins: [
+                'transform-decorators-legacy',
+                'transform-export-extensions',
+                'syntax-export-extensions'
+            ]
+        }
+    },
+    plugins: {
     }
-  },
-  plugins: {
-  }
 }
 
 if (prod) {
 
-  delete module.exports.compilers.babel.sourcesMap;
-  // 压缩sass
-  // module.exports.compilers['sass'] = {outputStyle: 'compressed'}
+    delete module.exports.compilers.babel.sourcesMap;
+    // 压缩sass
+    // module.exports.compilers['sass'] = {outputStyle: 'compressed'}
 
-  // 压缩less
-  module.exports.compilers['less'] = {compress: true}
+    // 压缩less
+    module.exports.compilers['less'] = { compress: true }
 
-  // 压缩js
-  module.exports.plugins = {
-    uglifyjs: {
-      filter: /\.js$/,
-      config: {
-      }
-    },
-    imagemin: {
-      filter: /\.(jpg|png|jpeg)$/,
-      config: {
-        jpg: {
-          quality: 80
+    // 压缩js
+    module.exports.plugins = {
+        uglifyjs: {
+            filter: /\.js$/,
+            config: {
+            }
         },
-        png: {
-          quality: 80
+        imagemin: {
+            filter: /\.(jpg|png|jpeg)$/,
+            config: {
+                jpg: {
+                    quality: 80
+                },
+                png: {
+                    quality: 80
+                }
+            }
         }
-      }
     }
-  }
 }
